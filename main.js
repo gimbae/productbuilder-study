d01eab2e1461cb87b07894d0fd59e9143e0ed557
gimbae87 <gimbae87@gmail.com>
2026-01-19 09:53:57 +0000

    Refactor: HTML, CSS, JS 파일 분리

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-// Add JS here
+function togglePepperoni() {
+      const wrap = document.getElementById('pepperoni-wrap');
+      wrap.style.display =
+        wrap.style.display === 'none' || wrap.style.display === ''
+          ? 'flex'
+          : 'none';
+    }
